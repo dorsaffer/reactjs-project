5489c4395696be487dfae1dceebe5f7100fdac81
Dorsaf Ferchichi <dorsaf.ferchichi@esprit.tn>
2020-08-15 23:12:35 +0100

    displaying the selected dish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './components/shared/dishes'
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+  CardTitle } from 'reactstrap';
 
-function App() {
+ class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      dishes:DISHES,
+
+    }
+
+
+  }
+
+
+  render(){
   return (
     <div className="App">
     <Navbar dark color="primary">
@@ -10,8 +26,11 @@ function App() {
         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
       </div>
     </Navbar>
+    <Menu dishes={this.state.dishes}></Menu>
   </div>
+
   );
+}
 }
 
 export default App;
